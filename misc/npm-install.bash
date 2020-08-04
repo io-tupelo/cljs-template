87b98d0c056de1db42c09f546472e5e7da0e85d0
Alan Thompson <cloojure@gmail.com>
2020-08-04 15:58:25 -0700

    misc

--- misc/npm-install.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -v
-# npm install phantomjs    # does not support current React
-# npm install slimerjs     # does not support current Firefox
-npm install karma karma-cljs-test  --save-dev
-npm install karma-junit-reporter   --save-dev
-npm install karma-chrome-launcher  karma-firefox-launcher  karma-safari-launcher  --save-dev
